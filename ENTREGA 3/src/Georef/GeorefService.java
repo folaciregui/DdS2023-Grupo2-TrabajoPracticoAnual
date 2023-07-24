83f77fa1b8ba2bcf6d6e64621369852987078f59
aylencandela <aylencandela@hotmail.com>
2023-07-24 20:25:03 -0300

    add: api georef

---
+++ ENTREGA 3/src/Georef/GeorefService.java
@@ -0,0 +1,24 @@
+package Georef;
+
+import Georef.entidadesMolde.*;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+//interfaz que utiliza retrofit para matchear con la api, se definen las request http que pueden ir a georef despues de la url base
+public interface GeorefService {
+
+    //falta departamentos
+
+    @GET("provincias")
+    Call<ListadoDeProvincias> provincias();
+
+    @GET("provincias")
+    Call<ListadoDeProvincias> provincias (@Query("campos") String campos);
+
+    @GET("municipios")
+    Call<ListadoDeMunicipios> municipios (@Query("provincia") int provinciaId);
+
+    @GET("departamentos")
+    Call<ListadoDeDepartamentos> departamentos (@Query("provincia") int provinciaId);
+}
---
+++ ENTREGA 3/src/Georef/ServicioGeorefRetrofitAdapter.java
@@ -0,0 +1,50 @@
+package Georef;
+
+import Georef.entidadesMolde.*;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+
+//clase que trae los datos de la api con retrofit
+public class ServicioGeorefRetrofitAdapter implements APIGeorefAdapter {
+    private static final int maximaCantidadRegistrosDefault = 200;
+    private final String urlApi = "https://apis.datos.gob.ar/georef/api/";
+    private final Retrofit retrofit;
+
+    public ServicioGeorefRetrofitAdapter() {
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl(urlApi)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    //falta listaDepartamentos()
+
+    @Override
+    public ListadoDeProvincias listaProvincias() throws IOException {
+        GeorefService georefservice = this.retrofit.create(GeorefService.class);
+        Call<ListadoDeProvincias> requestProvincias = georefservice.provincias();
+        Response<ListadoDeProvincias> responseProvincias = requestProvincias.execute();
+        return responseProvincias.body();
+    }
+
+    @Override
+    public ListadoDeMunicipios listaMunicipios(int provinciaId) throws IOException {
+        GeorefService georefservice = this.retrofit.create(GeorefService.class);
+        Call<ListadoDeMunicipios> requestMunicipios = georefservice.municipios(provinciaId);
+        Response<ListadoDeMunicipios> responseMunicipios = requestMunicipios.execute();
+        return responseMunicipios.body();
+    }
+
+    @Override
+    public ListadoDeDepartamentos listaDepartamentos(int provinciaId) throws IOException {
+        GeorefService georefservice = this.retrofit.create(GeorefService.class);
+        Call<ListadoDeDepartamentos> requestDepartamentos = georefservice.departamentos(provinciaId);
+        Response<ListadoDeDepartamentos> responseDepartamentos = requestDepartamentos.execute();
+        return responseDepartamentos.body();
+    }
+
+}
