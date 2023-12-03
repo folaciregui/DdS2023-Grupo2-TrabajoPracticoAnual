aa48fe3d4dae3d8fe36a18192dcf831649347127
FranciscoGonzalez03 <frangonzalez@frba.utn.edu.ar>
2023-12-03 20:18:43 -0300

    tratando de arreglar fetch y controller

--- ENTREGA 6/src/main/java/com/example/hibernate/Main.java
+++ ENTREGA 6/src/main/java/com/example/hibernate/Main.java
@@ -9,19 +9,38 @@ import controllers.ServicioController;
 
 public class Main {
     public static void main(String[] args) {
-        // Configurar Javalin
-        Javalin app = Javalin.create(config -> {
-            config.registerPlugin(new HibernatePlugin(ConfigHibernate::new));
-        }).start(7000);
+        try {
+            // Configurar Javalin
+            Javalin app = Javalin.create(config -> {
+                config.registerPlugin(new HibernatePlugin(ConfigHibernate::new));
+            }).start(3000);
+            System.out.println("Servidor Javalin iniciado correctamente en el puerto 3000 antes de los get");
+            // Rutas
+            alert("JAVALIN CREADO");
+            app.get("/api/entidades", EntidadController::getEntidades);
+            app.get("/api/entidades/:entidadId/establecimientos", EstablecimientoController::getEstablecimientos);
+            app.get("/api/establecimientos/:establecimientoId/servicios", ServicioController::getServicios);
+            app.get("/api/entidades/:entidadId/establecimientos", ctx -> {
+                // Obtener el parámetro de la URL (entidadId)
+                String entidadId = ctx.pathParam("entidadId");
+                
+                // Lógica para obtener los establecimientos de la entidad con ID entidadId
+                // ...
+            
+                // Devolver la lista de establecimientos como JSON
+                ctx.json(listaDeEstablecimientos);
+            });
+            
 
-        // Rutas
-        app.get("/api/entidades", EntidadController::getEntidades);
-        app.get("/api/entidades/:entidadId/establecimientos", EstablecimientoController::getEstablecimientos);
-        app.get("/api/establecimientos/:establecimientoId/servicios", ServicioController::getServicios);
+            // Detener la aplicación al cerrar
+            //app.events(event -> event.serverStopping(ConfigHibernate::closeSessionFactory));
 
-        // Otros controladores y rutas...
-
-        // Detener la aplicación al cerrar
-        app.events(event -> event.serverStopping(ConfigHibernate::closeSessionFactory));
+            // Mensaje de registro indicando que el servidor se inició correctamente
+            System.out.println("Servidor Javalin iniciado correctamente en el puerto 3000");
+        } catch (Exception e) {
+            // Si hay algún error al iniciar el servidor, imprime un mensaje de error
+            e.printStackTrace();
+            System.err.println("Error al iniciar el servidor Javalin: " + e.getMessage());
+        }
     }
 }
--- ENTREGA 6/src/main/java/controllers/EntidadController.java
+++ ENTREGA 6/src/main/java/controllers/EntidadController.java
@@ -1,9 +1,24 @@
 package controllers;
 
+import java.util.Arrays;
 import io.javalin.http.Context;
+import java.util.List;
 
 public class EntidadController {
+
     public static void getEntidades(Context ctx) {
-        // Lógica para obtener y devolver la lista de entidades
+        // Lógica para obtener las entidades desde tu backend
+        // Por ejemplo:
+        List<String> listaDeEntidades = obtenerListaDeEntidades();
+
+        // Devolver las entidades como JSON
+        ctx.json(listaDeEntidades);
+    }
+
+    private static List<String> obtenerListaDeEntidades() {
+        // Lógica para obtener las entidades desde tu base de datos o en memoria
+        // Por ejemplo:
+        return List.of("Entidad1", "Entidad2", "Entidad3");
     }
 }
+
